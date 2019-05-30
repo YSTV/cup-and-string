7735e75180648ce641951a141a2323db024d0c89
Thomas Lee <thomasslee97@gmail.com>
2019-05-31 00:36:30 +0100

    Add config loading from file

--- cmd/talkiepi/main.go
+++ cmd/talkiepi/main.go
@@ -3,26 +3,74 @@ package main
 import (
 	"crypto/rand"
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/dchote/gumble/gumble"
-	_ "github.com/dchote/gumble/opus"
-	"github.com/dchote/talkiepi"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/dchote/gumble/gumble"
+	_ "github.com/dchote/gumble/opus"
+	"github.com/dchote/talkiepi"
 )
+
+// ConfigFile stores values from config file
+type ConfigFile struct {
+	Server      string `json:"server"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Insecure    bool   `json:"insecure"`
+	Certificate string `json:"certificate"`
+	Channel     string `json:"channel"`
+}
 
 func main() {
 	// Command line flags
-	server := flag.String("server", "talkiepi.projectable.me:64738", "the server to connect to")
+	server := flag.String("server", "", "the server to connect to")
 	username := flag.String("username", "", "the username of the client")
 	password := flag.String("password", "", "the password of the server")
 	insecure := flag.Bool("insecure", true, "skip server certificate verification")
 	certificate := flag.String("certificate", "", "PEM encoded certificate and private key")
-	channel := flag.String("channel", "talkiepi", "mumble channel to join by default")
+	channel := flag.String("channel", "", "mumble channel to join by default")
+	config := flag.String("config", "", "path to config file (takes priority over flags)")
 
 	flag.Parse()
+
+	/**
+	 * Config priority:
+	 * 	1. Config File
+	 *	2. Command line args
+	 */
+
+	if *config != "" {
+		fmt.Println(*config)
+		jsonFile, err := os.Open(*config)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer jsonFile.Close()
+
+		byteValue, err := ioutil.ReadAll(jsonFile)
+
+		var configFile ConfigFile
+
+		json.Unmarshal(byteValue, &configFile)
+		fmt.Println(configFile.Server)
+		fmt.Println(configFile.Username)
+		fmt.Println(configFile.Password)
+		fmt.Println(configFile.Insecure)
+		fmt.Println(configFile.Certificate)
+		fmt.Println(configFile.Channel)
+
+		*server = configFile.Server
+		*username = configFile.Username
+		*password = configFile.Password
+		*insecure = configFile.Insecure
+		*certificate = configFile.Certificate
+		*channel = configFile.Channel
+	}
 
 	// Initialize
 	b := talkiepi.Talkiepi{
